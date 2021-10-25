d2c82e6212fcc1c7e912e04004b4715a3dfbad02
Pedro Schwarz Rodrigues <pedroschawarz97@gmail.com>
2021-10-24 22:04:10 -0300

    Initial commit

---
+++ Theaterfy-iOS/Theaterfy_iOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  Theaterfy_iOSApp.swift
+//  Theaterfy-iOS
+//
+//  Created by Pedro Rodrigues on 24/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Theaterfy_iOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Theaterfy-iOSUITests/Theaterfy_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Theaterfy_iOSUITestsLaunchTests.swift
+//  Theaterfy-iOSUITests
+//
+//  Created by Pedro Rodrigues on 24/10/21.
+//
+
+import XCTest
+
+class Theaterfy_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
