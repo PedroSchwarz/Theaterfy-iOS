3d4d2603610577e71b4fdf92147dbc0b3bbe8f1c
Pedro Schwarz Rodrigues <pedroschawarz97@gmail.com>
2021-10-26 19:35:44 -0300

    Refactor ServerConstants and add path to GetMovies request

--- Theaterfy-iOS/app/constants/ServerConstants.swift
+++ Theaterfy-iOS/app/constants/ServerConstants.swift
@@ -10,4 +10,21 @@ import Foundation
 struct ServerConstants {
     static let BASE_URL = "https://api.themoviedb.org/3"
     static let API_KEY = "6ca7974eb20d621615c81a17d4234c53"
+    
+    struct GetNowPlaying {
+        static let PATH = "/movie/now_playing"
+    }
+    
+    static func buildPath(base: String = ServerConstants.BASE_URL, path: String, queryStrings: [String]? = nil) -> String {
+        var builtQuery: String = "?api_key=\(ServerConstants.API_KEY)"
+        
+        if let queries = queryStrings {
+            queries.forEach { query in
+               builtQuery += "\(query)\(queries.last != query ? "&" :  "")"
+           }
+        }
+        
+        print("\(base)\(path)\(builtQuery)")
+        return "\(base)\(path)\(builtQuery)"
+    }
 }
--- Theaterfy-iOS/features/movies/data/datasources/MoviesRemoteDataSource.swift
+++ Theaterfy-iOS/features/movies/data/datasources/MoviesRemoteDataSource.swift
@@ -16,7 +16,10 @@ struct MoviesRemoteDataSourceImpl : MoviesRemoteDataSource {
     var server: Server
     
     func getMovies(_ page: Int) -> AnyPublisher<[MovieModel], Failure> {
-        return server.execute(path: "", decodable: PaginationModel<MovieModel>.self)
+        return server.execute(
+            path: ServerConstants.buildPath(path: ServerConstants.GetNowPlaying.PATH),
+            decodable: PaginationModel<MovieModel>.self
+        )
             .map({ result in
                 result.results
             })
